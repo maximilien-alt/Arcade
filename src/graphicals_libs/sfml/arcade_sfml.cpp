7c43f072951775d086818f7db97757df1276d879
Jules MINGUET <jules.minguet@epitech.eu>
2021-03-09 14:43:33 +0100

    sfml

--- src/graphicals_libs/sfml/arcade_sfml.cpp
+++ src/graphicals_libs/sfml/arcade_sfml.cpp
@@ -1,28 +1,26 @@
 /*
 ** EPITECH PROJECT, 2021
-** bootstrap_arcade
+** B-OOP-400-MPL-4-1-arcade-leo.sarochar
 ** File description:
-** lib1
+** arcade_SFML
 */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "arcade_sfml.hpp"
 
-__attribute__((constructor))
-void constructor()
+Arcade::Graphical_SFML::Graphical_SFML() : AGraphicalModule()
 {
-    printf("[libfoo] Loading foo library...\n");
 }
 
-__attribute__((destructor))
-void destructor()
+Arcade::Graphical_SFML::~Graphical_SFML()
 {
-    printf("[libfoo] foo closing...\n");
 }
 
-extern "C" int entryPoint(void)
+void Arcade::Graphical_SFML::drawText(graphical_text_t &text)
 {
-    printf("[libfoo] Entry point for foo!\n");
-    return (0);
+    std::cout << "Jules" << std::endl;
+}
+
+extern "C" Arcade::IGraphicalModule *entryPoint()
+{
+    return new Arcade::Graphical_SFML;
 }
