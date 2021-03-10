523278971f47ca07884cc65cd80e53e5fd2094a4
Léo Sarochar <leo.sarochar@epitech.eu>
2021-03-10 10:28:01 +0100

    Adding sdl2 window openning

---
+++ src/graphicals_libs/sdl2/arcade_sdl2.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2021
+** B-OOP-400-MPL-4-1-arcade-leo.sarochar
+** File description:
+** arcade_sdl2
+*/
+
+#ifndef ARCADE_SDL2_HPP_
+#define ARCADE_SDL2_HPP_
+
+#include "../../core/AGraphicalModule.hpp"
+#include <SDL2/SDL.h>
+
+namespace Arcade
+{
+    class Graphical_SDL2 : public AGraphicalModule {
+        public:
+            Graphical_SDL2();
+            ~Graphical_SDL2();
+            void drawText(graphical_text_t &text) final;
+            void clear() final;
+            void refresh() final;
+            int check() final;
+            void openWindow() final;
+            void closeWindow() final;
+        
+        private:
+            WINDOW *_window;
+
+            int getInput();
+    };
+} // namespace Arcade
+
+#endif /* !ARCADE_SDL2_HPP_ */
